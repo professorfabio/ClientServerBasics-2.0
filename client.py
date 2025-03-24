5260e7f6fb199a976ab32ffcba39cc5d92a16500
Fábio M. Costa <fmc@ufg.br>
2025-03-24 09:12:31 -0300

    Update client.py
    Signed-off-by: Fábio M. Costa <fmc@ufg.br>

--- client.py
+++ client.py
@@ -1,9 +1,18 @@
 from socket  import *
 from constCS import * #-
+import pickle
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
+op = input("Operation to invoke: ")
+v1 = int(input("Enter 1st operand: "))
+v2 = int(input("Enter 2nd operand: "))
+data = {"OP":op, "V1":v1, "V2":v2}
+msg = pickle.dumps(data)
+packet = str.encode(msg)
+s.send(packet)  # send data packet
+packet = s.recv(1024)     # receive the response
+msg = bytes.decode(packet)
+data = pickle.loads(msg)
+print ("Result: ", data)            # print the result
 s.close()               # close the connection
