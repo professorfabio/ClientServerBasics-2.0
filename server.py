4c7be928cb47e369729c30e0147ff7c31d769ef7
Fábio M. Costa <fmc@ufg.br>
2025-03-24 09:02:55 -0300

    Update server.py
    Signed-off-by: Fábio M. Costa <fmc@ufg.br>

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 from socket  import *
 from constCS import * #-
+import pickle
 
 s = socket(AF_INET, SOCK_STREAM) 
 s.bind((HOST, PORT))  #-
 s.listen(1)           #-
 (conn, addr) = s.accept()  # returns new socket and addr. client 
 while True:                # forever
-  data = conn.recv(1024)   # receive data from client
-  if not data: break       # stop if client stopped
-  print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+  packet = conn.recv(1024)    # receive data from client
+  if not packet: break        # stop if client stopped
+  msg = bytes.decode(packet) 
+  print(msg)
+  data = pickle.loads(msg)
+  print(data)
+  if data[0] == "SUM":
+    data = data[1] + data[2]
+    msg = pickle.dumps(data)
+    packet = str.encode(msg)
+    conn.send(packet)
 conn.close()               # close the connection
