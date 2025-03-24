b68d46dbcfaa7dcf58a169904b4004766cf09d28
Fábio M. Costa <fmc@ufg.br>
2025-03-24 11:36:47 -0300

    Update client.py
    Signed-off-by: Fábio M. Costa <fmc@ufg.br>

--- client.py
+++ client.py
@@ -9,10 +9,13 @@ v1 = int(input("Enter 1st operand: "))
 v2 = int(input("Enter 2nd operand: "))
 data = {"OP":op, "V1":v1, "V2":v2}
 msg = pickle.dumps(data)
-#packet = str.encode(msg)
 s.send(msg)  # send data packet
 msg = s.recv(1024)     # receive the response
-#msg = bytes.decode(packet)
 data = pickle.loads(msg)
-print ("Result: ", data)            # print the result
+if data["STATUS"] == "OK":
+  print ("Result: ", data["RES"])            # print the result
+elif data["STATUS"] == "NOK" and data["RES"] == 1:
+  print ("Operation does not exist.")
+else:
+  print ("Resultado inesperado.")
 s.close()               # close the connection
