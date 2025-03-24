07238205a22381de1897fa8de5d11e88a7987090
Fábio M. Costa <fmc@ufg.br>
2025-03-24 11:32:47 -0300

    Update server.py
    Signed-off-by: Fábio M. Costa <fmc@ufg.br>

--- server.py
+++ server.py
@@ -8,14 +8,21 @@ s.listen(1)           #-
 (conn, addr) = s.accept()  # returns new socket and addr. client 
 while True:                # forever
   msg = conn.recv(1024)    # receive data from client
-  if not msg: break        # stop if client stopped
-  #msg = bytes.decode(packet) 
+  if not msg: break        # stop if client stopped 
   print(msg)
   data = pickle.loads(msg)
   print(data)
   if data["OP"] == "sum":
-    data = data["V1"] + data["V2"]
-    msg = pickle.dumps(data)
-    #packet = str.encode(msg)
-    conn.send(msg)
+    res = data["V1"] + data["V2"]
+    status = "OK"
+  elif data["OP"] == "sub":
+    res = data["V1"] - data["V2"]
+    status = "OK"
+  else:
+    status = "NOK"
+    res = 1
+  data = ["STATUS":status, "RES":res]
+  msg = pickle.dumps(data)
+  conn.send(msg)
+  
 conn.close()               # close the connection
