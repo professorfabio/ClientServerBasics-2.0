b3cb7d94a75ba095ee454b838b181dd0238b8e58
Fábio M. Costa <fmc@ufg.br>
2025-04-14 10:46:45 -0300

    Update server.py with threads
    Signed-off-by: Fábio M. Costa <fmc@ufg.br>

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from socket  import *
-from constCS import * #-
+from threading import Thread
 import pickle
+from random import *
+from constCS import * #-
 
 s = socket(AF_INET, SOCK_STREAM) 
 s.bind((HOST, PORT))  #-
@@ -12,6 +14,13 @@ while True:                # forever
   print(msg)
   data = pickle.loads(msg)
   print(data)
+  num = randint(1,100)
+  t = Thread (target=execute, args=(data,num))
+  t.start()
+conn.close()               # close the connection
+
+def execute(data, num):
+  print ("Thread: " + num)
   if data["OP"] == "sum":
     res = data["V1"] + data["V2"]
     status = "OK"
@@ -24,5 +33,4 @@ while True:                # forever
   data = {"STATUS":status, "RES":res}
   msg = pickle.dumps(data)
   conn.send(msg)
-  
-conn.close()               # close the connection
+  return
