7b05c65ce7b76c956ac7e0dc1b52e3cfb8223f95
Fábio M. Costa <fmc@ufg.br>
2025-09-22 13:52:39 -0300

    Update client.py
    Signed-off-by: Fábio M. Costa <fmc@ufg.br>

--- client.py
+++ client.py
@@ -3,7 +3,6 @@ from constCS import * #-
 import pickle
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
 op = input("Operation to invoke: ")
 v1 = int(input("Enter 1st operand: "))
 v2 = int(input("Enter 2nd operand: "))
@@ -11,6 +10,7 @@ data = {"OP":op, "V1":v1, "V2":v2}
 msg = pickle.dumps(data)
 
 for i in range(100):
+  s.connect((HOST, PORT)) # connect to server (block until accepted)
   s.send(msg)  # send data packet
   msg = s.recv(1024)     # receive the response
   data = pickle.loads(msg)
@@ -22,7 +22,11 @@ for i in range(100):
     print ("Server terminated.")
   else:
     print ("Unexpected result.")
+  s.close()
+  
+# Last message to close the server
 data["OP"] = "fim"
 msg = pickle.dumps(data)
+s.connect((HOST, PORT)) # connect to server (block until accepted)
 s.send(msg)
 s.close()               # close the connection
